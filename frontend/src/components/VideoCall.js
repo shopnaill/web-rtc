6d97e5c3e2654c47cc2ff5e569bb9ec90acc111d
shopnaill <mohamed1999250@gmail.com>
2024-12-11 22:34:03 +0200

    fix

--- frontend/src/components/VideoCall.js
+++ frontend/src/components/VideoCall.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("ws://rtc.gym-engine.com:3000" ,  {
+const socket = io("wss://rtc.gym-engine.com:3000" ,  {
     transports: ["websocket"]
   });
 
