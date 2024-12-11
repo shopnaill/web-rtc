80a3bbda7dcf704b3797f95f08d4c4c0c85c06a5
shopnaill <mohamed1999250@gmail.com>
2024-12-11 22:21:21 +0200

    fix

--- frontend/src/components/VideoCall.js
+++ frontend/src/components/VideoCall.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("wss://rtc.gym-engine.com:3000", {
+const socket = io("http://rtc.gym-engine.com:3000", {
   transports: ["websocket"], // Make sure to use the WebSocket transport
 });
 
